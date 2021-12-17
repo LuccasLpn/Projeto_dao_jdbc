c1b54280161f19200598d28cbf673289924b283f
Luccas Pereira <Luccas.lpn@outlook.com>
2021-12-17 01:14:49 -0300

    Seller entity Class

---
+++ src/model/entities/Seller.java
@@ -0,0 +1,118 @@
+package model.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+public class Seller implements Serializable {
+    
+    
+    private static final long serialVersionUID = 1l;
+    
+    private Integer id;
+    private String name;
+    private String email;
+    private Date birDate;
+    private Double BaseSalary;
+    
+    private Department department;
+
+    public Seller() {
+    }
+
+    public Seller(Integer id, String name, String email, Date birDate, Double BaseSalary, Department department) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.birDate = birDate;
+        this.BaseSalary = BaseSalary;
+        this.department = department;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getBirDate() {
+        return birDate;
+    }
+
+    public void setBirDate(Date birDate) {
+        this.birDate = birDate;
+    }
+
+    public Double getBaseSalary() {
+        return BaseSalary;
+    }
+
+    public void setBaseSalary(Double BaseSalary) {
+        this.BaseSalary = BaseSalary;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 59 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Seller other = (Seller) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Seller{" + "id=" + id + ", name=" + name + ", email=" + email + ", birDate=" + birDate + ", BaseSalary=" + BaseSalary + ", department=" + department + '}';
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
