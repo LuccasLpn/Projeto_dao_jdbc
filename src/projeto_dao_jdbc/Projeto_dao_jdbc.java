05bf1b13c1c8670f94fa6c10c39913e53f93f35c
Luccas Pereira <Luccas.lpn@outlook.com>
2021-12-17 01:06:47 -0300

    Department entity class

--- src/projeto_dao_jdbc/Projeto_dao_jdbc.java
+++ src/projeto_dao_jdbc/Projeto_dao_jdbc.java
@@ -1,21 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package projeto_dao_jdbc;
 
-/**
- *
- * @author Lucca
- */
-public class Projeto_dao_jdbc {
+import model.entities.Department;
 
-    /**
-     * @param args the command line arguments
-     */
+public class Projeto_dao_jdbc {
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+        
+    Department obj = new Department(1, "Books");
+        System.out.println(obj);
+        
+        
+        
+        
     
+}
 }
