f0d44d22e3695cd9a94141eff44f591e0ebdfb4a
Luccas Pereira <Luccas.lpn@outlook.com>
2021-12-17 21:08:47 -0300

    update implementation

--- src/projeto_dao_jdbc/Projeto_dao_jdbc.java
+++ src/projeto_dao_jdbc/Projeto_dao_jdbc.java
@@ -15,12 +15,20 @@ public class Projeto_dao_jdbc {
     Seller seller = sellerDao.findById(3);
     System.out.println(seller);
     
+    System.out.println("");
+    System.out.println("------------------------------------------------------------------------------------");
+    
     System.out.println("\n === TEST 2: seller findByDepartment ====");
     Department department = new Department(2, null);
     List<Seller> list = sellerDao.findByDepartment(department);
     for(Seller obj: list){
         System.out.println(obj);
     }
+        
+    
+    System.out.println("");
+    System.out.println("------------------------------------------------------------------------------------");
+    
         System.out.println("\n === TEST 3: seller findAll ====");
     
                 list = sellerDao.findAll();
@@ -28,10 +36,22 @@ public class Projeto_dao_jdbc {
                     System.out.println(obj);
                 }
                 
+    System.out.println("");
+    System.out.println("------------------------------------------------------------------------------------");            
+                
     System.out.println("=== TEST 4: seller Insert ====");
     Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
     sellerDao.insert(newSeller);
     System.out.println("Inserted! New id = " + newSeller.getId());
+    
+    System.out.println("");
+    System.out.println("------------------------------------------------------------------------------------");
+    
+    System.out.println("=== TEST 5: seller Update ==== ");
+    seller = sellerDao.findById(1);
+    seller.setName("Marthe Wayne");
+    sellerDao.update(seller);
+    System.out.println("Update completed");
     
     
     
